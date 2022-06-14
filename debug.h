1426b5b9f46a60456afea5b99825abbee6cae542
NickChiapputo <chiapputo.nick@gmail.com>
2022-06-14 10:20:36 -0500

    Added debug preprocessor for changing data types.

--- debug.h
+++ debug.h
@@ -1,7 +1,10 @@
 #ifndef DEBUG_H_
 #define DEBUG_H_
 
-#define DEBUG 1
-#define DEBUG_PRESTRING "> "
+#define DEBUG 0
+#define DEBUG_PRESTRING ""
+
+// Ctrl+f highlight all change back when done.
+#define DATATYPE float
 
 #endif
