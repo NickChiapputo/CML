404809921468e1437f9d3e298d637cfbcb5a8a6d
NickChiapputo <chiapputo.nick@gmail.com>
2022-06-14 10:21:31 -0500

    Implemented 2D convolution with padding.

--- layers.c
+++ layers.c
@@ -1,10 +1,97 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "debug.h"
 #include "layers.h"
 
-int LAYERS_load_weights( double ***** weights, double ** bias, 
+
+int LAYERS_conv2d_constructor( LAYERS_Conv2D * layer, 
+    uint16_t inFilters, uint16_t outFilters,
+    uint16_t kernelWidth, uint16_t kernelHeight,
+    uint16_t strideX, uint16_t strideY,
+    uint16_t padded, 
+    char * weightsFile,
+    int (*forward)(DATATYPE****, DATATYPE***, uint16_t, uint16_t, uint16_t, LAYERS_Conv2D) )
+{
+    // TODO: Check for input validity.
+
+
+    // Set the input and output filter numbers.
+    (*layer).inFilters = inFilters;
+    (*layer).outFilters = outFilters;
+
+    // Set the kernel size.
+    (*layer).kernelWidth = kernelWidth;
+    (*layer).kernelHeight = kernelHeight;
+
+    // Set the kernel stride.
+    (*layer).strideX = strideX;
+    (*layer).strideY = strideY;
+
+    // Set flag to determine if input is padded or not.
+    // If zero, no padding. 
+    // If one, padding is used (output is same size as input).
+    (*layer).padded = padded;
+
+    // If weights file is provided, load in weights.
+    if( weightsFile != NULL )
+    {
+        // Intialize the kernel weights and biases.
+        LAYERS_load_weights( &(*layer).weights, &(*layer).bias,
+            inFilters, outFilters,
+            kernelHeight, kernelWidth,
+            weightsFile );
+    }
+
+    // Define the forward function.
+    (*layer).forward = forward;
+
+    return 0;
+}
+
+
+int LAYERS_prelu_constructor( LAYERS_PreLU * layer,
+    uint16_t outFilters, char * weightsFile,
+    int (*forward)(DATATYPE****, DATATYPE***, uint16_t, uint16_t, LAYERS_PreLU) )
+{
+    // TODO: Check for input validity.
+
+    
+    // Set the output filter numbers.
+    (*layer).outFilters = outFilters;
+
+    // If weights file is provided, load in weights.
+    if( weightsFile != NULL )
+    {
+        // Intialize the ReLU weights.
+        LAYERS_prelu_load_weights( &(*layer).weights, outFilters, weightsFile );
+    }    
+
+    // Define the forward function.
+    (*layer).forward = forward;
+
+    return 0;
+}
+
+
+int LAYERS_relu_constructor( LAYERS_ReLU * layer, uint16_t outFilters,
+    int (*forward)(DATATYPE****, DATATYPE***, uint16_t, uint16_t, LAYERS_ReLU) )
+{
+    // TODO: Check for input validity.
+
+    
+    // Set the output filter numbers.
+    (*layer).outFilters = outFilters;  
+
+    // Define the forward function.
+    (*layer).forward = forward;
+
+    return 0;
+}
+
+
+int LAYERS_load_weights( DATATYPE ***** weights, DATATYPE ** bias, 
     uint16_t inC, uint16_t outC, 
     uint16_t filterHeight, uint16_t filterWidth,
     char * filename )
@@ -19,41 +106,60 @@ int LAYERS_load_weights( double ***** weights, double ** bias,
         * To print weights from Python using Tensorflow, first store 
         * weights and bias in list 'weights'. Then, use the following to print:
 
-for z in range( outC ):
-  for k in range( inC ):
-    for i in range( kernelHeight ):
-      for j in range( kernelWidth ):
-        print( f"{weights[ 0 ][ i ][ j ][ k ][ z ]}", end=' ' if j != kernelWidth - 1 else '\n' )
-  print( f"{weights[ 1 ][ z ]}" )
+        layer = model.model.layers[ 1 ]
+
+        with open( f"{save_location}{first_conv_layer.name}", "wb" ) as f:
+            # Original shape: kernelHeight x kernelWidth x inputChannels x outputChannels
+            # New shape:      kernelHeight x outputChannels x inputChannels x kernelWidth
+            # This is done so that when the structs are written to the file, it does
+            # so across each row (e.g., all columns in first row, all columns in 
+            # second row, and so on). Prints floats in little-endian 
+            # IEEE.754 32-bit format.
+            weights = np.swapaxes( layer[ 0 ], 1, 3 )
+
+            for z in range( outC ):
+                for k in range( inC ):
+                    for i in range( kernelHeight ):
+                        f.write( struct.pack( 'f' * len( weights[ i ][ z ][ k ] ), 
+                            *weights[ i ][ z ][ k ] ) )
+
+                # Write biases to file.
+                f.write( struct.pack( 'f' * len( np.array( [layer[ 1 ][ z ]] ) ), 
+                    *np.array( [layer[ 1 ][ z ]] ) ) )
 
     ****/ 
 
     // Allocate kernels for weights and biases for each output channel.
-    (*weights) = (double****) malloc( outC * sizeof( double*** ) );
-    (*bias) = (double*) malloc( outC * sizeof( double ) );
+    (*weights) = (DATATYPE****) malloc( outC * sizeof( DATATYPE*** ) );
+    (*bias) = (DATATYPE*) malloc( outC * sizeof( DATATYPE ) );
+    float val;
     for( uint32_t z = 0; z < outC; z++ )
     {
         // Allocate kernel weights for each input channel.
-        (*weights)[ z ] = (double***) malloc( inC * sizeof( double** ) );
+        (*weights)[ z ] = (DATATYPE***) malloc( inC * sizeof( DATATYPE** ) );
         for( uint32_t k = 0; k < inC; k++ )
         {
             // Allocate rows for each kernel.
-            (*weights)[ z ][ k ] = (double**) malloc( filterHeight * sizeof( double* ) );
+            (*weights)[ z ][ k ] = (DATATYPE**) malloc( filterHeight * sizeof( DATATYPE* ) );
             for( uint16_t i = 0; i < filterHeight; i++ )
             {
                 // Allocate values for each kernel.
-                (*weights)[ z ][ k ][ i ] = (double*) malloc( filterWidth * sizeof( double ) );
+                (*weights)[ z ][ k ][ i ] = (DATATYPE*) malloc( filterWidth * sizeof( DATATYPE ) );
 
                 for( uint16_t j = 0; j < filterWidth; j++ )
                 {
-                    // Read in kernel weights for current input/output channel combination.
-                    fscanf( weightsfp, "%lf", &(*weights)[ z ][ k ][ i ][ j ] );
+                    // Read in kernel weights for current input/output channel 
+                    // combination. Weights are stored in 32-bit floating point
+                    // little-endian format. 
+                    fread( (void*) (&val), sizeof( val ), 1, weightsfp );
+                    (*weights)[ z ][ k ][ i ][ j ] = (DATATYPE)val;
                 }
             }
         }
 
         // Read in bias value for current output channel.
-        fscanf( weightsfp, "%lf", &(*bias)[ z ] );
+        fread( (void*) (&val), sizeof( val ), 1, weightsfp );
+        (*bias)[ z ] = val;
     }
 
     fclose( weightsfp );
@@ -61,7 +167,60 @@ for z in range( outC ):
     return 0;
 }
 
-int LAYERS_convolution_2d( double **** output, double *** input, uint16_t inY,
+
+int LAYERS_prelu_load_weights( DATATYPE ** weights, uint16_t outC, 
+    char * filename )
+{
+    FILE * weightsfp = fopen( filename, "r" );
+    if( weightsfp == NULL )
+    {
+        return 1;
+    }
+
+    (*weights) = (DATATYPE*) malloc( outC * sizeof( DATATYPE ) );
+    float val;
+    for( uint16_t z = 0; z < outC; z++ )
+    {
+        fread( (void*) (&val), sizeof( val ), 1, weightsfp );
+        (*weights)[ z ] = (DATATYPE)val;
+    }
+
+    fclose( weightsfp );
+
+    return 0;
+}
+
+
+int LAYERS_conv2d_free_weights( LAYERS_Conv2D * layer )
+{
+    for( uint16_t z = 0; z < (*layer).outFilters; z++ )
+    {
+        for( uint16_t k = 0; k < (*layer).inFilters; k++ )
+        {
+            for( uint16_t y = 0; y < (*layer).kernelHeight; y++ )
+            {
+                free( (*layer).weights[ z ][ k ][ y ] );
+            }
+            free( (*layer).weights[ z ][ k ] );
+        }
+        free( (*layer).weights[ z ] );
+    }
+
+    free( (*layer).weights );
+    free( (*layer).bias );
+
+    return 0;
+}
+
+
+int LAYERS_prelu_free_weights( LAYERS_PreLU * layer )
+{
+    free( (*layer).weights );
+
+    return 0;
+}
+
+int LAYERS_convolution_2d( DATATYPE **** output, DATATYPE *** input, uint16_t inY,
                            uint16_t inX, uint16_t inC, LAYERS_Conv2D layer )
 {
     // Calculate the output size.
@@ -92,7 +251,7 @@ int LAYERS_convolution_2d( double **** output, double *** input, uint16_t inY,
     int16_t i, j, k,   // Iterate over kernel.
             y, x, z,   // Iterate over output.
             yy, xx;    // Index input.
-    double sum;         // Calculate convolution.
+    DATATYPE sum;         // Calculate convolution.
 
 
     // Calculate necessary padding (if needed).
@@ -110,17 +269,29 @@ int LAYERS_convolution_2d( double **** output, double *** input, uint16_t inY,
     uint16_t leftPadding = widthPadding / 2;
     // uint16_t rightPadding = widthPadding - leftPadding;
 
+
+    for( z = 0; z < layer.outFilters; z++ )
+        for( y = 0; y < outY; y++ )
+            for( x = 0; x < outX; x++ )
+                (*output)[ z ][ y ][ x ] = layer.bias[ z ];
+
     for( z = 0; z < layer.outFilters; z++ ) // Output channels.
     {
-        printf( "Output Channel %i:\n", z+1 );
+        #if DEBUG
+            printf( "Output Channel %i:\n", z+1 );
+        #endif
 
         for( k = 0; k < inC; k++ )  // Input channels.
         {
-            printf( "  Input Channel %i:\n", k+1 );
+            #if DEBUG
+                printf( "  Input Channel %i:\n", k+1 );
+            #endif
 
             for( i = 0; i < outY; i += layer.strideY )  // Output rows.
             {
-                printf( "  " );
+                #if DEBUG
+                    printf( "  " );
+                #endif
                 for( j = 0; j < outX; j += layer.strideX )  // Output columns.
                 {
                     sum = 0.0;
@@ -161,7 +332,7 @@ int LAYERS_convolution_2d( double **** output, double *** input, uint16_t inY,
                             if( yy < 0 || yy >= inY ||
                                 xx < 0 || xx >= inX )
                             {
-                                sum += 0;
+                                // sum += 0
                             }
                             else
                             {
@@ -172,23 +343,61 @@ int LAYERS_convolution_2d( double **** output, double *** input, uint16_t inY,
                         }
                     }
 
-                    printf( "  % 5.2f", sum );
+                    #if DEBUG
+                        printf( "  % 5.2f", sum );
+                    #endif
                     (*output)[ z ][ i ][ j ] += sum;
                 }
 
-                printf( "\n" );
+                #if DEBUG
+                    printf( "\n" );
+                #endif
             }
         }
 
-        printf( "\n" );
+        #if DEBUG
+            printf( "\n" );
+        #endif
     }
 
+    return 0;
+}
 
-    for( z = 0; z < layer.outFilters; z++ )
-        for( y = 0; y < outY; y++ )
-            for( x = 0; x < outX; x++ )
-                (*output)[ z ][ y ][ x ] += layer.bias[ z ];
 
+int LAYERS_prelu_forward( DATATYPE **** output, DATATYPE *** input, 
+                          uint16_t inY, uint16_t inX, LAYERS_PreLU layer )
+{
+    for( uint16_t z = 0; z < layer.outFilters; z++ )
+    {
+        for( uint16_t y = 0; y < inY; y++ )
+        {
+            for( uint16_t x = 0; x < inX; x++ )
+            {
+                (*output)[ z ][ y ][ x ] = input[ z ][ y ][ x ] > 0 ? 
+                    input[ z ][ y ][ x ] : 
+                    input[ z ][ y ][ x ] * layer.weights[ z ];
+            }
+        }
+    }
+
+    return 0;
+}
+
+
+int LAYERS_relu_forward( DATATYPE **** output, DATATYPE *** input, 
+                          uint16_t inY, uint16_t inX, LAYERS_ReLU layer )
+{
+    for( uint16_t z = 0; z < layer.outFilters; z++ )
+    {
+        for( uint16_t y = 0; y < inY; y++ )
+        {
+            for( uint16_t x = 0; x < inX; x++ )
+            {
+                if( input[ z ][ y ][ x ] < 0 )
+                    (*output)[ z ][ y ][ x ] = 0;
+            }
+        }
+    }
 
     return 0;
 }
--- layers.h
+++ layers.h
@@ -8,11 +8,26 @@
 typedef struct 
 {
 	uint16_t numClasses;
-	double * weights;
-	double * bias;
-	int  (*activation)(double*, int, double*);
+	DATATYPE * weights;
+	DATATYPE * bias;
+	int  (*activation)(DATATYPE*, int, DATATYPE*);
 	void * transfer;
 } LAYERS_activation;
+
+typedef struct LAYERS_PreLU
+{
+	uint16_t outFilters;
+	DATATYPE * weights;
+	int (*forward)(DATATYPE****, DATATYPE***, uint16_t, uint16_t, 
+		struct LAYERS_PreLU);
+} LAYERS_PreLU;
+
+typedef struct LAYERS_ReLU
+{
+	uint16_t outFilters;
+	int (*forward)(DATATYPE****, DATATYPE***, uint16_t, uint16_t, 
+		struct LAYERS_ReLU);
+} LAYERS_ReLU;
 
 typedef struct LAYERS_Conv2D
 {
@@ -27,28 +42,56 @@ typedef struct LAYERS_Conv2D
 
 	uint8_t padded;
 
-	double **** weights;	// Shape = (outFilters)x(inFilters)x(kernelHeight)x(kernelWidth)
-	double *   bias;		// Shape = (inFiltesr * outFilters)x1
+	DATATYPE **** weights;	// Shape = (outFilters)x(inFilters)x(kernelHeight)x(kernelWidth)
+	DATATYPE *   bias;		// Shape = (inFiltesr * outFilters)x1
 
-	int (*forward)(double****, double***, uint16_t, uint16_t, 
+	int (*forward)(DATATYPE****, DATATYPE***, uint16_t, uint16_t, 
 				   uint16_t, struct LAYERS_Conv2D);
 
-	int (*activation)(double*, int, double*);
+	void * nextLayer;
 } LAYERS_Conv2D;
 
 
+/**** Layer Constructors ****/
+int LAYERS_conv2d_constructor( LAYERS_Conv2D * layer, 
+	uint16_t inFilters, uint16_t outFilters,
+	uint16_t kernelWidth, uint16_t kernelHeight,
+    uint16_t strideX, uint16_t strideY,
+	uint16_t padded, 
+	char * weightsFile,
+	int (*forward)(DATATYPE****, DATATYPE***, uint16_t, uint16_t, uint16_t, LAYERS_Conv2D) );
+
+int LAYERS_prelu_constructor( LAYERS_PreLU * layer,
+    uint16_t outFilters, char * weightsFile,
+    int (*forward)(DATATYPE****, DATATYPE***, uint16_t, uint16_t, LAYERS_PreLU) );
+
+int LAYERS_relu_constructor( LAYERS_ReLU * layer, uint16_t outFilters,
+    int (*forward)(DATATYPE****, DATATYPE***, uint16_t, uint16_t, LAYERS_ReLU) );
+
 /**** Helper Functions ****/
-int LAYERS_load_weights( double ***** weights, double ** bias, 
+int LAYERS_load_weights( DATATYPE ***** weights, DATATYPE ** bias, 
     uint16_t inC, uint16_t outC, 
     uint16_t filterHeight, uint16_t filterWidth,
     char * filename );
+
+int LAYERS_prelu_load_weights( DATATYPE ** weights, uint16_t outC, 
+	char * filename );
+
+int LAYERS_conv2d_free_weights( LAYERS_Conv2D * layer );
+
+int LAYERS_prelu_free_weights( LAYERS_PreLU * layer );
 
 
 /**** Forward Pass Functions ****/
 /****
  	*	@description
 ****/ 
-int LAYERS_convolution_2d( double **** output, double *** input, uint16_t inY,
+int LAYERS_convolution_2d( DATATYPE **** output, DATATYPE *** input, uint16_t inY,
 						   uint16_t inX, uint16_t inC, LAYERS_Conv2D layer );
 
+int LAYERS_prelu_forward( DATATYPE **** output, DATATYPE *** input, 
+						  uint16_t inY, uint16_t inX, LAYERS_PreLU layer );
+
+int LAYERS_relu_forward( DATATYPE **** output, DATATYPE *** input, 
+                          uint16_t inY, uint16_t inX, LAYERS_ReLU layer );
 #endif
